fda67f1330188887529972f14be4e56e28069fac
Chaitanya H <chaitanya.hanumanthu@gmail.com>
2023-03-12 16:20:06 +0530

    This branch contains all the admin api features

---
+++ controllers/admin.controller.js
@@ -0,0 +1,46 @@
+const expressAsyncHandler = require("express-async-handler");
+const { Concerns } = require("../models/concerns.model");
+const { Project } = require("../models/project.model");
+
+// route for getting all projects
+const getProjects = expressAsyncHandler(async (req, res) => {
+  let allProjects = await Project.findAll({
+    include: [
+      { association: Project.Updates },
+      { association: Project.Concerns },
+    ],
+  });
+  res.send({
+    message: "All the project details are here",
+    Projects: allProjects,
+  });
+});
+
+// route for getting the projects by id
+const getPorjectById = expressAsyncHandler(async (req, res) => {
+  let findProject = await Project.findOne({
+    where: {
+      projectId: req.params.projectId,
+    },
+    include: [
+      { association: Project.Updates },
+      { association: Project.Concerns },
+    ],
+  });
+
+  res.send({
+    message: "Th particular project details are: ",
+    Project: findProject,
+  });
+});
+
+// route for creating and assigning a project
+const createProject = expressAsyncHandler(async (req, res) => {
+  let projectCreation = await Project.create(req.body);
+  res.send({
+    message: "Project is created and assigned",
+    payload: projectCreation,
+  });
+});
+
+module.exports = { getProjects, createProject, getPorjectById };
